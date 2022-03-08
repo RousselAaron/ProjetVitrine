79c8f3a0a78a3110d19f6d97b6e27a9be0a4fee6
Aaron <roussel.aaron.tr@gmail.com>
2022-03-08 12:19:05 +0100

    composer & autoload fix

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@ require './vendor/autoload.php';
 require './src/Session/session_helper.php';
 
 
-$router = new App\Route\Router();
+$router = new \App\Route\Router();
 $router->getController();
 
 
